bb049a939cbeca73f087591138b6ea1a25f7b459
matteodev8 <59450459+matteodev8@users.noreply.github.com>
2022-07-09 14:54:38 +0200

    🏓 Ping command

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ import os
 dotenv.load_dotenv()
 
 bot = interactions.Client(token=os.environ["TOKEN"])
+gID = int(os.environ["DEV_GUILD"])
 
 @bot.event
 async def on_ready():
     print("Ready.")
+
+# I'll move this to a different folder later
+@bot.command(
+    name="ping",
+    description="Replies with the current latency.",
+    scope=gID,
+)
+async def ping(ctx: interactions.CommandContext):
+    await ctx.send(f"Pong! {round(bot.latency)}ms")
 
 bot.start()
